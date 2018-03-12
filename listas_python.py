3891fd1e457c96d7ee2f7199871d25efb80d8e1f
Camilo Yate <cyateya35439@universidadean.edu.co>
2018-03-12 15:14:54 -0500

    creando un ejercicio completo de listas en p`ython
    Signed-off-by: camilo <cyateya35439@universidadean.edu.co>

---
+++ listas_python.py
@@ -0,0 +1,106 @@
+
+
+
+#EJERCICIO PRACTICO EN PYTHON
+
+
+"""
+1. CAPTURAR UNA LISTA
+2. MOSTRAR UNA LISTA
+3. AGREGAR UN ELEMENTO A LA LISTA
+4. ELIMINAR UN ELEMENTO DE LA LISTA
+5. MODIFICAR UN ELEMENTO DE LA LISTA
+6. SALIR
+"""
+
+
+def capturar():
+    global lista
+    lista = []
+    n = int(input("Cuantos elemetos va a tener tu lista \n"))
+
+    for i in range(0,n):
+        print("Ingresa el elemento del indice", i)
+        elemento = input()
+        lista.insert(i, elemento )
+
+def mostrar():
+    print(lista)
+
+def agregar():
+    elemnto = input("Ingresa un elemento que desees agregar \n")
+
+    indice = int(input("Ingrese el indice donde deseas agregarlo \n"))
+
+    longuitud = int(len(lista))
+    if(indice >longuitud or indice < 0):
+        print("El indice debe estar entre 0 y", longuitud)
+    else:
+        lista.insert(indice,elemnto)
+        print("Elemento Agregado")
+
+#~DEFINIENDO LA FUNCION ELIMINAR
+
+def eliminar():
+    indice = int(input("Ingrese el indice del Elemento que desea eliminar"))
+
+    longuitud = int(len(lista))
+    if (indice > longuitud or indice < 0):
+        print("El indice debe estar entre 0 y", longuitud-1)
+    else:
+        del lista[indice]
+        print("Elemento Eliminado")
+
+#LA FUNCION MODIFICAR
+
+def modificar():
+    indice = int(input("Ingrese el indice del Elemento que desea Modificar \n"))
+
+    elemento = input("Ingrese el Nuevo valor de Elemento \n")
+    longuitud = int(len(lista))
+    if (indice > longuitud or indice < 0):
+        print("El indice debe estar entre 0 y", longuitud - 1)
+    else:
+        lista[indice] = elemento
+        print("Elemento Modificado")
+
+
+#LA FUNCION PRINCIPAL
+
+def principal():
+    opcion = "1"
+
+    while(opcion != 6):
+        print("1. CAPTURAR UNA LISTA")
+        print("2. MOSTRAR UNA LISTA")
+        print("3. AGREGAR UN ELEMENTO A LA LISTA")
+        print("4. ELIMINAR UN ELEMENTO DE LA LISTA")
+        print("5. MODIFICAR UN ELEMENTO DE LA LISTA")
+        print("6. SALIR")
+
+        opcion = int(input("Que deseas hacer \n"))
+        if(opcion == 1):
+            capturar()
+        elif(opcion == 2):
+            mostrar()
+        elif (opcion == 3):
+            agregar()
+        elif (opcion == 4):
+            eliminar()
+        elif (opcion == 5):
+            modificar()
+        elif (opcion == 6):
+            print("Programa Terminado")
+        else:
+            print("Opcion Incorrecta")
+
+
+
+#~LLAMANDO A LA FUNCION PRINCIPAL
+
+principal()
+
+
+
+
+
